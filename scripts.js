89d3ba29223bbe6fb5100dd676d2f76a377a140b
kAlipay <richardabelanio@gmail.com>
2023-04-14 16:40:30 +0800

    for update 4/14/2023

---
+++ scripts.js
@@ -0,0 +1,26 @@
+var typing=new Typed(".text", {
+    strings: [ "Web Designer", "Freelancer", "Graphics Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+  });
+   // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
